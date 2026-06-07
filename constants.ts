8bc3b29f52cf4cafb70c7c314dd10fb24d60475b
Abhinav-RM <abhinavrm26@gmail.com>
2026-06-07 17:51:05 +0530

    fix: update developer email address to personal gmail

--- constants.ts
+++ constants.ts
@@ -13,4 +13,4 @@ export const INITIAL_ACHIEVEMENTS: Achievement[] = [
 ];
 
 export const APP_VERSION = '1.0.1';
-export const DEV_EMAIL = 'abhinavrm.dev@gmail.com';
+export const DEV_EMAIL = 'abhinavrm26@gmail.com';
